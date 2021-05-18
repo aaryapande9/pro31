c2a72f4629a60174250b8cb42301c8a8f6b56598
aaryapande9 <76436101+aaryapande9@users.noreply.github.com>
2021-05-18 12:42:09 +0530

    Add files via upload

---
+++ Particles.js
@@ -0,0 +1,20 @@
+class Particles{
+    constructor(x,y,r){
+       var options ={
+           restitution:0.4
+       }
+       this.r = r
+       this.body = Bodies.circle(x,y,this.r,options);
+       this.color = color(random(0,255),random(0,255),random(0,255));
+
+       World.add(world,this.body);
+    }
+    display(){
+         var pos = this.body.position;
+         var angle = this.body.angle;
+
+        rotate(angle);
+        ellipseMode(RADIUS);
+        ellipse(0,0,pos.x,pos.y,this.r);
+    }
+}
